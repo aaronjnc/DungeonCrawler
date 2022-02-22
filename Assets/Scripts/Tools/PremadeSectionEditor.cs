ba7ed275bbdcbce652b9255a5b289ac5bd0e85e5
Aaron <aaronkingnc@att.net>
2022-02-22 12:28:02 -0500

    Began PremadeSectionEditor

---
+++ Assets/Scripts/Tools/PremadeSectionEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PremadeSection))]
+public class PremadeSectionEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        PremadeSection section = (PremadeSection)target;
+        if (GUILayout.Button("Generate Map"))
+        {
+
+        }
+    }
+}
