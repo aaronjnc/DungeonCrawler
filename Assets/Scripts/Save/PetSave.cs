649f7a872983c96c329ace0c66b1e245a56d9419
Aaron <aaronkingnc@att.net>
2022-04-13 18:17:09 -0400

    Market Assets

---
+++ Assets/Scripts/Save/PetSave.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PetSave
+{
+    [Tooltip("Monsters have been found")]
+    private bool[][] found;
+    
+    public PetSave(MonsterLog log)
+    {
+        List<MonsterLogEntry> logs = log.GetLogs();
+        found = new bool[logs.Count][];
+        for (int i = 0; i < logs.Count; i++)
+        {
+            MonsterLogEntry logEntry = logs[i];
+            //found
+        }
+    }
+}
